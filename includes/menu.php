9a0a3bf0c8898e944397fa6f01b422da2890cdb8
xuser123 <xuser123@jumi.biz>
2023-08-25 21:44:36 +0200

    move menu from index.html to menu.php

---
+++ includes/menu.php
@@ -0,0 +1,22 @@
+<div class="header-content">
+<h1 class="jumi-logo">
+<a href="https://www.jumi.biz">jumi.biz</a>&#32;<span id=jumi-path>
+<a href="https://www.jumi.biz/Content.php">/content</a>&#32;<?php echo $Pfad ?></span>
+</h1>
+<nav class="site-nav">
+      <button class="menu-toggle"
+          aria-controls="primary-navigation"
+          aria-expanded="false">
+    <span class="visually-hidden">Menu</span>
+    <div class="hamburger" aria-hidden="true"></div>
+  </button>
+  <ul data-state="closed" class="primary-navigation" id="primary-navigation">
+    <li><a href="https://www.jumi.biz/Content.php"><i class="site-nav--icon"></i>content</a></li>
+    <li><a href="https://www.jumi.biz/Links.php"><i class="site-nav--icon"></i>links</a></li>
+    <li><a href="https://www.jumi.biz/Notes.php"><i class="site-nav--icon"></i>notes</a></li>
+    <li><a href="https://www.jumi.biz/Cheats.php"><i class="site-nav--icon"></i>cheats</a></li>
+    <li><a href="https://www.jumi.biz/index.php"><i class="site-nav--icon"></i><span id="wtf">wtf is jumi?</span></a></li>
+  </ul>
+</nav>
+</div>
+<script src="includes/navibar.js"></script>
