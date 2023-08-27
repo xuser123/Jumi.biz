81950b9b5438e946128daec9dd95c861d1dd6d4b
xuser123 <xuser123@jumi.biz>
2023-08-27 03:56:25 +0200

    logical improvements for github-page

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-<h3 align="center">Website for my webpresence and contributions</h3>
-<p>This is the Github-Page-Version of <a href="https://www.jumi.biz/">jumi.biz</a>.<br> It contains guides, notes and random stuff.</p>
+<h3 align="center"><a href="https://jungmike.jumi.biz">jungmike.jumi.biz</a></h3>
+<p>This is the Github-Page-Version of <a href="https://www.jumi.biz/">jumi.biz</a>.<br> It contains guides, notes and what ever i want to contribute.</p>
--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <!DOCTYPE html><html lang="de">
 <head>
-
 <title>jumi.biz</title>
 
-<!-- # Metadaten -->
-<!-- # Favicon -->
-<!-- # Kodierung -->
-<!-- # HTTP-Header -->
-<!-- # Optimierung für Mobilgeräte -->
-	<?php readfile ('templates/basic.htm'); ?>
+<!-- ### Metadaten -->
+<?php readfile ('templates/basic.htm'); ?>
 
-<!-- # Layout -->
-	<?php readfile ('templates/layout.htm'); ?>
+<!-- ### Layout -->
+<?php readfile ('templates/layout.htm'); ?>
 
 </head>
 <body>
 <header>
 
 <!-- ### MENU -->
-	<?php readfile ('includes/menu.php'); ?>
+<?php readfile ('includes/menu.php'); ?>
 
 <!-- ### NAVIGATION -->
 	<script src="assets/js/menu.js"></script>
@@ -27,19 +22,18 @@
 <main>
 
 <!-- ### MAIN -->
-	<?php readfile ('includes/main.php'); ?>
+<?php readfile ('pages/main.php'); ?>
 
 </main>
 <footer>
 
 <!-- ### IMPRESSUM -->
-	<?php readfile ('includes/impressum.htm'); ?>
+<?php readfile ('includes/impressum.htm'); ?>
 
 </footer>
 
-<!-- # Modal-Script -->
+<!-- ### Extras -->
 	<script src="assets/js/modal.js"></script>
-<!-- # Syntax-Highlighting -->
     <script src="includes/extras/highlight.min.js"></script>
     <script>hljs.highlightAll();</script>
 
