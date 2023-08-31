a5d4792af372084101a4ad401611c1ff5effb09c
xuser123 <xuser123@jumi.biz>
2023-09-01 00:01:35 +0200

    Easteregg Planung vorbereitet, JavaScript-PHP-Combi noch unsicher

---
+++ assets/.hidden/hint.php
@@ -0,0 +1,77 @@
+<header>
+
+<!-- ### MENU -->
+<div class="header-content">
+
+<h1 class="jumi-logo">
+<a href="https://www.jumi.biz">jumi.biz</a>&#32;
+</h1>
+
+<nav class="site-nav">
+<button class="menu-toggle"
+        aria-controls="primary-navigation"
+        aria-expanded="false">
+  <span class="visually-hidden">Menu</span>
+  <div class="hamburger" aria-hidden="true"></div>
+</button>
+
+<ul data-state="closed" class="primary-navigation" id="primary-navigation">
+<li><a href="https://www.jumi.biz/Content"><i class="site-nav--icon"></i>content</a></li>
+<li><a href="https://www.jumi.biz/Links"><i class="site-nav--icon"></i>links</a></li>
+<li><a href="https://www.jumi.biz/Notes"><i class="site-nav--icon"></i>notes</a></li>
+<li><a href="https://www.jumi.biz/Cheats"><i class="site-nav--icon"></i>cheats</a></li>
+<li><a href="https://www.jumi.biz"><i class="site-nav--icon"></i><span id="wtf">wtf is jumi?</span></a></li>
+
+</ul>
+</nav>
+</div>
+
+<!-- ### NAVIGATION -->
+	<script src="../js/menu.js"></script>
+
+</header>
+<main>
+
+<!-- ### MAIN -->
+<div class="grid-container">
+<article class="content flow">
+
+<!-- ### EASTEREGG -->
+<pre id="enter-code-here">
+
+  ____  __ __  ___ ___  ____      ____   ____  _____
+ |    ||  |  ||   |   ||    |    |    \ |    ||     |
+ |__  ||  |  || _   _ | |  |     |  o  ) |  | |__/  |
+ __|  ||  |  ||  \_/  | |  |     |     | |  | |   __|
+/  |  ||  :  ||   |   | |  |  __ |  O  | |  | |  /  |
+\  `  ||     ||   |   | |  | |  ||     | |  | |     |
+ \____| \__,_||___|___||____||__||_____||____||_____|
+
+
+```py
+CODE = int(EASTEREGG)
+```
+
+```sh
+echo '3A57323GG' > 3AST3R3GG; cat 3AST3R3GG > 3ASTEREGG
+$3ASTEREGG=34573R366; echo '$3ASTEREGG' > 3AST3R3GG
+cat 3AST3R3GG > 3A57323GG; EASTEREGG=$(&lt;3A57323GG)
+```
+
+</pre>
+
+</div>
+
+</div>
+
+</main>
+<footer>
+
+<!-- ### IMPRESSUM -->
+<span class="email-link">contact@jumi.biz</span>
+
+</footer>
+
+<!-- ### Extras -->
+	<script src="assets/js/modal.js"></script>
+    <script src=".secret.js"></script>
